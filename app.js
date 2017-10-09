20489b17015dcc2d525c4887c92ec6ce5b9134b2
Code Politan <codepolitan@gmail.com>
2017-10-09 13:16:47 +0200

    Use path

--- app.js
+++ app.js
@@ -3,23 +3,25 @@
  * @type {[type]}
  */
 
-var compression = require('compression')
-var express = require('express')
-var app = express()
+var __dirname
 
-var port = 3001
+const path = require('path')
+const compression = require('compression')
+const express = require('express')
+const app = express()
+
+const port = 3001
 
 app.use(compression())
 // app.use('/', express.static(__dirname + '/dist'));
-app.use('/dist', express.static(__dirname + '/dist'))
-app.use('/public', express.static(__dirname + '/public'))
-app.use('/src', express.static(__dirname + '/src'))
-app.use('/vendor', express.static(__dirname + '/dist/vendor'))
+app.use('/dist', express.static(path.join(__dirname, '/dist')))
+app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/vendor', express.static(path.join(__dirname, '/dist/vendor')))
 
-app.use('/material', express.static(__dirname + '/node_modules/material/dist'))
-app.use('/report', express.static(__dirname + '/node_modules/material/report'))
-app.use('/docs', express.static(__dirname + '/node_modules/material/docs'))
-app.use('/doc', express.static(__dirname + '/node_modules/material/doc'))
+app.use('/material', express.static(path.join(__dirname, '/node_modules/material/dist')))
+app.use('/report', express.static(path.join(__dirname, '/node_modules/material/report')))
+app.use('/docs', express.static(path.join(__dirname, '/node_modules/material/docs')))
+app.use('/doc', express.static(path.join(__dirname, '/node_modules/material/doc')))
 
 app.listen(process.env.PORT || port)
 
