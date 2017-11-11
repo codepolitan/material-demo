f445098e0793c8c28ef9685e9d96ae8a78e0e851
Code Politan <codepolitan@gmail.com>
2017-11-11 06:33:04 +0100

    Remove public

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
  * @type {[type]}
  */
 
-var __dirname
-
 const path = require('path')
 const compression = require('compression')
 const express = require('express')
@@ -13,15 +11,13 @@ const app = express()
 const port = 3001
 
 app.use(compression())
-// app.use('/', express.static(__dirname + '/dist'));
+app.use('/', express.static(path.join(__dirname, '/')))
 app.use('/dist', express.static(path.join(__dirname, '/dist')))
 app.use('/public', express.static(path.join(__dirname, '/public')))
-app.use('/vendor', express.static(path.join(__dirname, '/dist/vendor')))
 
 app.use('/material', express.static(path.join(__dirname, '/node_modules/material/dist')))
 app.use('/report', express.static(path.join(__dirname, '/node_modules/material/report')))
 app.use('/docs', express.static(path.join(__dirname, '/node_modules/material/docs')))
-app.use('/doc', express.static(path.join(__dirname, '/node_modules/material/doc')))
 
 app.listen(process.env.PORT || port)
 
