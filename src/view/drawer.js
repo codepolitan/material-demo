3acf2331ba23d896db45a0b712c4c0a768daceab
Code Politan <codepolitan@gmail.com>
2018-07-16 17:00:52 +0200

    enhance drawer

--- src/view/drawer.js
+++ src/view/drawer.js
@@ -36,94 +36,17 @@ export default function (body) {
       [Text, 'title', { type: 'title', text: 'Buttons' }],
       [Text, 'title', { text: 'Buttons communicate the action that will occur when the user touches them.' }]
     ],
-    [Container, 'hero', {},
-      [Toolbar, 'head', { display: 'flex', direction: 'horizontal', color: 'primary' },
-        [Button, 'menu-navi', { icon: iconNavi, type: 'action' }],
-        [Text, 'title', { text: TITLE }],
-        [Button, 'menu-more', { icon: iconMore, type: 'action' }]
-      ]
-    ],
     [Container, 'sample', {},
       [Container, 'top', {},
-        [Text, 'title', { type: 'title', text: 'Normal' }]
-      ],
-      [Container, 'screen', {},
-        [Toolbar, 'head', { display: 'flex', direction: 'horizontal', color: 'primary' },
-          [Button, 'menu-navi', { icon: iconNavi, type: 'action' }],
-          [Text, 'title', { text: 'Title' }],
-          [Button, 'menu-more', { icon: iconMore, type: 'action' }]
-        ],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }]
-      ]
-    ],
-    [Container, 'sample', {},
-      [Container, 'top', {},
-        [Text, 'title', { type: 'title', text: 'Fixed' }]
-      ],
-      [Container, 'screen', { css: 'fixed-screen' },
-        [Toolbar, 'head', { fixed: 'fixed', color: 'primary' },
-          [Button, 'menu-navi', { icon: iconNavi, type: 'action' }],
-          [Text, 'title', { text: 'Title' }],
-          [Button, 'menu-more', { icon: iconMore, type: 'action' }]
-        ],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }]
-      ]
-    ],
-    [Container, 'sample', {},
-      [Container, 'top', {},
-        [Text, 'title', { type: 'title', text: 'Flexile' }]
-      ],
-      [Container, 'screen', { css: 'flexile-screen' },
-        [Toolbar, 'head', { type: 'flexible', height: 224, display: 'flex', direction: 'vertical', color: 'primary' },
-          [Container, 'section', { css: 'menu', display: 'flex', direction: 'horizontal' },
-            [Button, 'menu-navi', { icon: iconNavi, type: 'action' }],
-            [Divider, 'section', { flex: 1 }],
-            [Button, 'menu-apps', { icon: iconApps, type: 'action' }],
-            [Button, 'menu-more', { icon: iconMore, type: 'action' }]
-          ],
-          [Container, 'section', { },
-            [Text, 'title', { css: 'pin-bottom', type: 'title', text: 'Title' }]
-          ]
-        ],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }],
-        [Text, '', { text: CONTENT }]
-
-      ]
-    ],
-    [Container, 'sample', {},
-      [Container, 'top', {},
-        [Text, 'title', { type: 'title', text: 'Flexible' }]
+        [Text, 'title', { type: 'title', text: 'Temporary' }]
       ],
       [Container, 'screen', { css: 'screen-waterfall' },
-        [Toolbar, 'waterfalltollbar', { waterfall: 1, height: 224, flexible: 1, fixed: 1, display: 'flex', direction: 'vertical', color: 'primary' },
-          [Container, 'section', { css: 'menu', display: 'flex', direction: 'horizontal' },
-            [Button, 'menu-water', { icon: iconNavi, type: 'action' }],
-            [Divider, 'section', { flex: 1 }],
-            [Button, 'menu-apps', { icon: iconApps, type: 'action' }],
-            [Button, 'menu-more', { icon: iconMore, type: 'action' }]
-          ],
-          [Container, 'section', { },
-            [Text, 'title', { css: 'pin-bottom', type: 'title', text: 'Title' }]
-          ]
+        [Toolbar, 'head', { fixed: 'fixed', color: 'primary' },
+          [Button, 'navi-temporary', { icon: iconNavi, type: 'action' }],
+          [Text, 'title', { text: 'Title' }],
+          [Button, 'menu-more', { icon: iconMore, type: 'action' }]
         ],
-        [Drawer, 'navi-water', { display: 'flex', direction: 'vertical', css: 'drawer-temporary', type: 'temporary' },
+        [Drawer, 'drawer-temporary', { display: 'flex', direction: 'vertical', css: 'drawer-temporary', type: 'temporary' },
           [Toolbar, 'navi-head', { type: 'app' },
             [Button, 'menu-navi-head', { icon: iconNavi, type: 'action' }],
             [Text, 'title', { text: TITLE }]
@@ -139,12 +62,113 @@ export default function (body) {
         [Text, '', { text: CONTENT }]
 
       ]
+    ],
+    [Container, 'sample', {},
+      [Container, 'top', {},
+        [Text, 'title', { type: 'title', text: 'persistent' }]
+      ],
+      [Container, 'screen', { css: 'screen-persistent' },
+
+        [Drawer, 'drawer-persistent', { display: 'flex', direction: 'vertical', css: 'drawer-persistent', type: 'persistent' },
+          [Toolbar, 'navi-head', { },
+            [Button, 'menu-navi-head', { icon: iconNavi, type: 'action' }],
+            [Text, 'title', { text: TITLE }]
+          ],
+          [List, 'navi-list', { flex: '1' }]
+        ],
+        [Container, 'content', { css: 'content-persistent' },
+          [Toolbar, 'head', { display: 'flex', direction: 'horizontal', color: 'primary' },
+            [Button, 'navi-persistent', { icon: iconNavi, type: 'action' }],
+            [Text, 'title', { text: 'Title' }],
+            [Button, 'menu-more', { icon: iconMore, type: 'action' }]
+          ],
+          [Container, 'text-content', { css: 'content-text' },
+            [Text, '', { text: CONTENT }],
+            [Text, '', { text: CONTENT }],
+            [Text, '', { text: CONTENT }],
+            [Text, '', { text: CONTENT }],
+            [Text, '', { text: CONTENT }],
+            [Text, '', { text: CONTENT }],
+            [Text, '', { text: CONTENT }]
+          ]
+        ]
+      ]
+    ],
+    [Container, 'sample', {},
+      [Container, 'top', {},
+        [Text, 'title', { type: 'title', text: 'permanent' }]
+      ],
+      [Container, 'screen', { css: 'screen-permanent' },
+        [Drawer, 'drawer-permanent', { display: 'flex', direction: 'vertical', css: 'drawer-permanent', type: 'permanent' },
+          [Toolbar, 'navi-head', { },
+            [Text, 'title', { text: TITLE }]
+          ],
+          [List, 'navi-list', { flex: '1' }]
+        ],
+        [Container, 'main', { css: 'container-main' },
+          [Toolbar, 'head', { display: 'flex', direction: 'horizontal', color: 'primary' },
+            [Button, 'navi-permanent', { icon: iconNavi, type: 'action' }],
+            [Text, 'title', { text: 'Title' }],
+            [Button, 'menu-more', { icon: iconMore, type: 'action' }]
+          ],
+          [Container, 'content', { css: 'content-permanent' },
+            [Container, 'text-content', { css: 'content-text' },
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }]
+            ]
+          ]
+        ]
+      ]
+    ],
+    [Container, 'sample', {},
+      [Container, 'top', {},
+        [Text, 'title', { type: 'title', text: 'permanent' }]
+      ],
+      [Container, 'screen', { css: 'screen-permanent' },
+        [Toolbar, 'head', { display: 'flex', direction: 'horizontal', color: 'primary' },
+          [Text, 'title', { text: 'Title' }],
+          [Button, 'menu-more', { icon: iconMore, type: 'action' }]
+        ],
+        [Container, 'main', { css: 'container-main' },
+          [Drawer, 'drawer-permanent', { display: 'flex', direction: 'vertical', css: 'drawer-permanent', type: 'permanent' },
+            [List, 'navi-list', { flex: '1' }]
+          ],
+          [Container, 'content', { css: 'content-permanent' },
+            [Container, 'text-content', { css: 'content-text' },
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }],
+              [Text, '', { text: CONTENT }]
+            ]
+          ]
+        ]
+      ]
     ]
   ], body)
 
-  var navi = layout.get('navi-water')
+  var temporary = layout.get('drawer-temporary')
 
-  layout.get('menu-water').on('click', function (e) {
-    navi.toggle(e)
+  layout.get('navi-temporary').on('click', function (e) {
+    temporary.toggle(e)
+  })
+
+  var persistent = layout.get('drawer-persistent')
+
+  layout.get('navi-persistent').on('click', function (e) {
+    persistent.toggle(e)
+  })
+
+  var permanent = layout.get('drawer-permanent')
+
+  layout.get('navi-permanent').on('click', function (e) {
+    permanent.toggle(e)
   })
 }
